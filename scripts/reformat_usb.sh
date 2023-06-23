fc62338185533d09b92b603538282060d3e67fb2
Ryan Kramer <ryrykray@gmail.com>
2023-06-23 17:30:53 -0600

    reformat usb script

---
+++ scripts/reformat_usb.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Exit on non-zero status
+set -e
+
+# Treat unset variables as an error and exit
+set -u
+
+# Help function
+Help()
+{
+    echo "List available drives: sudo fdisk -l"
+    echo "Using this script: sudo $0 <drive>"
+}
+
+# Check for no input arguments
+if [ "$#" -eq  "0" ]; then
+    Help
+    exit 1
+fi
+
+# Reformat drive from input argument
+drive="$1"
+mkfs.ext4 "${drive}"
